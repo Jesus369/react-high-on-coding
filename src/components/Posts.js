baa1fd0dcdf9a2623909e00f6b47f580f9291633
Jesus Flores <31966603+Jesus369@users.noreply.github.com>
2018-01-22 20:56:36 -0600

    Add files via upload

---
+++ src/components/Posts.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+export class Posts extends Component {
+  render() {
+
+    var container = {
+      marginBottom: "2em"
+    }
+
+    var title = {
+      color: "red",
+      margin: "0 0 .7em 4em",
+      fontSize: "1.3em"
+    }
+
+    var comments_likes = {
+      backgroundColor: "#E3E3E3",
+      display: "flex",
+      flexDirection: "row",
+      listStyle: "none",
+      margin: "0 auto 0 auto",
+      width:"90%"
+    }
+
+    var type = {
+      margin: "0 auto 0 auto"
+    }
+
+    var listing = {
+      margin: "0 auto 0 auto",
+      width: "90%"
+    }
+
+    return (
+      <div style={container}>
+        <div style={title}>{this.props.title}</div>
+        <div style={listing}>{this.props.post}</div>
+        <div style={comments_likes}>
+          <li style={type}>{this.props.comments} Comments</li>
+          <li style={type}>{this.props.likes} Likes</li>
+        </div>
+      </div>
+    )
+  }
+}
